f8af67c6d4642bd1972b9c01e92c3958a1543dc1
anushkachouhan029 <anushkachouhan029>
2022-12-03 22:03:03 +0530

    added power of two

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Basic_Core_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the power value");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= exponent; i++)
+            {
+                Console.WriteLine("2^{0} " + " " + "= {1}", i, Math.Pow(2, i));
+            }
+        }
+    }
+}
